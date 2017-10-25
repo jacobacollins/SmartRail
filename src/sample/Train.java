d71f74db4a5f0dec87bd0feaafaac7ff7ccd31a8
Jacob Collins <jacobacollins123@gmail.com>
2017-10-25 01:07:10 -0600

    Made train that moves across screen. Just basic right now

--- src/sample/Train.java
+++ src/sample/Train.java
@@ -1,43 +1,61 @@
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
-/**
- * Created by Vincent on 10/15/2017.
- */
-public class Train
-{
+import java.util.Random;
 
-  private Rectangle train = new Rectangle();
-  private int position;
-  private Color trainColor;
-  private boolean moving = false;
-  public Train(int position, Color traincolor)
-  {
-    this.trainColor = traincolor;
-    train.setX(25);
-    train.setY(25);
-    train.setFill(traincolor);
-    this.position = position;
-  }
+public class Train extends TrackObject implements Runnable {
+    private Rectangle train = new Rectangle();
+    private boolean moving = false;
+    private int x, y = 25;
+    private Canvas canvas;
+    private GraphicsContext gc;
+    private boolean flag = false;
 
-  public void moveRight()
-  {
-    position++;
-  }
 
-  public void moveLeft()
-  {
-    position--;
-  }
+    public Train(String TrainID, Canvas canvas) {
+        super(TrainID, false);
+        train.setX(25);
+        train.setY(25);
+        this.canvas = canvas;
+        gc = canvas.getGraphicsContext2D();
+    }
 
-  public void setMoving(boolean moving)
-  {
-    this.moving = moving;
-  }
 
-  public boolean isMoving() {
-    return moving;
-  }
+    private void changeCoordinates() {
+        x += 25;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void run() {
+        while (!flag) {
+            try {
+                Thread.sleep(500);
+
+                if (getX() < 500) {
+                    gc.clearRect(getX(), getY(), 10, 10);
+                    changeCoordinates();
+                    gc.fillRect(getX(), getY(), 10, 10);
+                } else {
+                    flag = true;
+                }
+
+                System.out.println(getX() + " " + getY());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
 
 
 }
