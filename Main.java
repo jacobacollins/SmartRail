5c81dce46526fe540c1546ba12db75bc5e0b2b64
Jacob Collins <jacobacollins123@gmail.com>
2017-11-02 22:30:31 -0600

    version 2 dfs

---
+++ Main.java
@@ -0,0 +1,86 @@
+package sample;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Hello World");
+
+        BorderPane root = new BorderPane();
+        root.setStyle("-fx-background-color:darkkhaki");
+        //left Pane
+
+        VBox leftPane = new VBox();
+
+        leftPane.setPadding(new Insets(50,25,50,25));
+        leftPane.setSpacing(100);
+        leftPane.setAlignment(Pos.BASELINE_LEFT);
+        Label trainNumber = new Label("# of trains");
+        trainNumber.setFont(new Font(17));
+        Label activeLights = new Label("# of active lights");
+        activeLights.setFont(new Font(17));
+        Label activeSwitches = new Label("# of active switches");
+        activeSwitches.setFont(new Font(17));
+        leftPane.getChildren().addAll(trainNumber, activeLights,activeSwitches);
+
+        root.setLeft(leftPane);
+
+        StackPane canvasPane = new StackPane();
+
+        Canvas middle = new Canvas(800,350);
+        canvasPane.getChildren().add(middle);
+        canvasPane.setStyle("-fx-background-color: gainsboro");
+        canvasPane.setAlignment(Pos.CENTER);
+        root.setCenter(canvasPane);
+        BorderPane.setMargin(canvasPane, new Insets(25,25,25,25));
+//      GraphicsContext gc = middle.getGraphicsContext2D();  
+
+        TrackObject[][] layout = new TrackObject[3][10];
+//        layout = [1][11]
+      TrackLayout tl = new TrackLayout(layout);
+      LayoutDisplay layoutDisplay = new LayoutDisplay(middle);
+
+
+
+
+      layoutDisplay.tracksDisplay(layout);
+//  ConductorScreen inpute = new ConductorScreen(Thread.currentThread());
+
+      Train t2 = new Train( "blah", middle, layout[0][1], "U", 1);
+
+
+      Thread t1 = new Thread( t2);
+
+      t1.start();
+
+
+        primaryStage.setScene(new Scene(root, 1100, 400));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    }
+
---
+++ Path.java
@@ -0,0 +1,101 @@
+package sample;
+import java.util.ArrayList;
+
+/**
+ * Created by Vincent on 10/20/2017.
+ */
+
+public class Path
+{
+
+    private TrackObject currentTrack, firstTrack;
+    private String destination;
+
+    public Path(TrackObject currentTrack, String destination){
+      this.currentTrack = currentTrack;
+      this.destination = destination;
+      firstTrack = currentTrack;
+
+      buildPath(currentTrack, destination);
+  }
+
+  private ArrayList<TrackObject> path = new ArrayList<>();
+
+  public void addToPath(TrackObject track)
+  {
+    path.add(track);
+  }
+
+  public void removePath(Track track)
+  {
+    path.clear();
+  }
+
+  private void buildPath(TrackObject currentTrack, String destination) {
+
+
+      if (!currentTrack.getRightNeighbor().isVisited()) {
+          if (currentTrack.getRightNeighbor().getID().equals("track") || currentTrack.getRightNeighbor().getID().equals("light")) {
+              addToPath(currentTrack);
+
+
+              currentTrack = currentTrack.getRightNeighbor();
+              buildPath(currentTrack, destination);
+          } else if (currentTrack.getRightNeighbor().getID().equals("urs")
+                  || currentTrack.getRightNeighbor().getID().equals("drs")
+                  || currentTrack.getRightNeighbor().getID().equals("uls")
+                  || currentTrack.getRightNeighbor().getID().equals("dls")) {
+
+              switch (currentTrack.getRightNeighbor().getID()) {
+                  case "urs":
+                      break;
+                  case "drs":
+
+
+                      if (!currentTrack.getRightNeighbor().getBottomNeighbor().isVisited()) {
+                          addToPath(currentTrack);
+                          addToPath(currentTrack.getRightNeighbor());
+                          currentTrack = currentTrack.getRightNeighbor().getBottomNeighbor();
+                          buildPath(currentTrack, destination);
+                      } else {
+
+
+                          addToPath(currentTrack);
+                          currentTrack = currentTrack.getRightNeighbor();
+                          buildPath(currentTrack, destination);
+
+                      }
+                      break;
+                  case "dls":
+                      break;
+                  case "uls":
+                      break;
+
+              }
+          } else if (currentTrack.getRightNeighbor().getID().equals(destination)) {
+              addToPath(currentTrack);
+              addToPath(currentTrack.getRightNeighbor());
+              return;
+          } else if(!currentTrack.getRightNeighbor().getID().equals(destination)) {
+
+          addToPath(currentTrack);
+          addToPath(currentTrack.getRightNeighbor());
+          currentTrack.setVisited(true);
+              path.clear();
+              buildPath(firstTrack, destination);
+          return;
+      }
+      } else {
+          currentTrack.setVisited(true);
+          path.clear();
+          buildPath(firstTrack, destination);
+      }
+  }
+
+
+
+  public ArrayList<TrackObject> getPath()
+  {
+    return path;
+  }
+}
